a3b838b120d368a5504b28d07e056efdc2704644
Faizan <faizanansari4862@gmail.com>
2024-01-08 23:24:41 +0530

    db chnnged

--- database/database.js
+++ database/database.js
@@ -1,9 +1,6 @@
-// microservices-ecommerce/database/database.js
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/e-commerce", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+
+mongoose.connect("mongodb://127.0.0.1:27017/e-commerce");
 
 const productSchema = new mongoose.Schema({
   name: String,
