ae7e531ce3ec0774f625af7e5a0395fe2658913d
Faizan <faizanansari4862@gmail.com>
2024-02-14 23:23:00 +0530

    testing with mongo URI

--- backend/backend.js
+++ backend/backend.js
@@ -82,8 +82,10 @@ app.delete("/api/products/:id", async (req, res) => {
 
 // old uri = mongodb://127.0.0.1:27017/e-commerce
 
+// docker-compose = mongodb://database:27017/e-commerce
+
 mongoose
-  .connect("mongodb://database:27017/e-commerce")
+  .connect("mongodb://127.0.0.1:27017/e-commerce")
   .then(async () => {
     console.log("Connected to the database");
     await checkAndInsertDemoData(); // Check and insert demo data
