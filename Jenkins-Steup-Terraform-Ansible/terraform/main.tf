1f6cf42b5487499692dc7b6c747469719c725ae5
faizan35 <faizanansari4862@gmail.com>
2024-04-09 17:28:23 +0530

    SG added

--- Jenkins-Steup-Terraform-Ansible/terraform/main.tf
+++ Jenkins-Steup-Terraform-Ansible/terraform/main.tf
@@ -20,7 +20,57 @@ module "eks-vpc" {
 }
 
 # SG
+module "sg-eks" {
+  source = "terraform-aws-modules/security-group/aws"
 
+  name        = "SG-Jenkins-EKS"
+  description = "Security group for Jenkins"
+  vpc_id      = module.eks-vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      description = "Jenkins ports"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      description = "HTTP"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      description = "HTTPS"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "SSH"
+      cidr_blocks = "0.0.0.0/0"
+    },        
+  ]
+  egress_with_cidr_blocks = [
+    {
+        from_port   = 0
+        to_port     = 0 
+        protocol    = "-1"
+        description = "Allow all outbound Traffic"
+        cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  tags = {
+    Name = "Jenkins-ESK-SG"
+  }
+}
 
 
 # EC2
