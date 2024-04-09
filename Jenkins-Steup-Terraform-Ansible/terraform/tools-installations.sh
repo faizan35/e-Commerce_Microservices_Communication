11f7419d6d008a99d32d329d663598d17cafd656
faizan35 <faizanansari4862@gmail.com>
2024-04-10 04:05:55 +0530

    yes flag added

--- Jenkins-Steup-Terraform-Ansible/terraform/tools-installations.sh
+++ Jenkins-Steup-Terraform-Ansible/terraform/tools-installations.sh
@@ -2,7 +2,7 @@
 
 # Installation of Java
 sudo apt update
-sudo apt install fontconfig openjdk-17-jre
+sudo apt install -y fontconfig openjdk-17-jre
 java -version
 openjdk version "17.0.8" 2023-07-18
 OpenJDK Runtime Environment (build 17.0.8+7-Debian-1deb12u1)
@@ -16,7 +16,7 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install -y jenkins
 
 
 
