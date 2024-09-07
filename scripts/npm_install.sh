9bae441bd5376cab114604eb8a1c7301376da910
faizan35 <faizanansari4862@gmail.com>
2024-09-07 21:26:29 +0530

    script updated

--- scripts/npm_install.sh
+++ scripts/npm_install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+cd ..
+
 cd backend/
 npm i
+echo "------------- Backend Installation Complete ----------------"
 
 cd ..
 
 cd frontend/
 npm i
+echo "------------- Frontend Installation Complete ----------------"
 
 cd ..
 
-echo "Installation Completed!"
+echo "----------------Installation Completed!----------------------"
